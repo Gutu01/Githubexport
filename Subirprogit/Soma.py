c44087a8085b20b7c68a18468e5ef0fe5f512668
Gutu01 <gustavomt058@gmail.com>
2025-09-02 20:59:22 -0400

    Terminei o primeiro exercício

---
+++ Subirprogit/Soma.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite um número:"))
+n2 = int(input("Digite o segundo número:"))
+
+media = (n1+n2)/2
+
+print("A média dos números é", media)
+
---
+++ Subirprogit/dados.py
@@ -0,0 +1,8 @@
+nome = input("Digite o nome completo:")
+idade = input("Digite sua idade:")
+sexo = input("Digite seu sexo:")
+endereco = input("Digite seu endereço:")
+telefone = input("Digite seu telefone:")
+
+print("\nNome completo:", nome, "\nIdade:",idade, "\nSeu sexo:", sexo,"\nEndereço:",endereco,"\nTelefone:", telefone)
+
