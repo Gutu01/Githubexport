cfb014b926af07b4449d3a1b5784aa06c3a71127
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-18 15:02:08 -0300

    função principal

--- Subirprogit/biblioteca2(Beta).c
+++ Subirprogit/biblioteca2(Beta).c
@@ -40,3 +40,28 @@ void LimparBufferEntrada(){
 }
 
 // --- Função Principal (main) ---
+int main() {
+    // 1. ALOCAÇÃO DINÂMICA DE MEMÓRIA
+    // Agora, em vez de arrays estáticos, usamos ponteiros.
+    struct Livro *biblioteca;
+    struct Emprestimo *emprestimos;
+
+    //Usamos calloc para o array de livros. calloc(num_elementos, tamanho_de_cada_elemento)
+    //Vantagem: inicializa toda a memória com zeros. Isso significa que 'disponivel' já começa com...
+    biblioteca = (struct Livro *) calloc(MAX_LIVROS, sizeof(struct Livro));
+
+    // Usamos malloc para o array de empréstimos. mallox(tamanho_total_em_bytes)
+    emprestimos = (struct Emprestimo *) malloc(MAX_EMPRESTIMOS * sizeof(struct Emprestimo));
+
+    // VERIFICAÇÃO: É crucial verificar se a aloxação de memória deu certo.
+    if (biblioteca == NULL || emprestimos == NULL){
+        printf("ERRO: Falha ao alocar memoria.\n");
+        return 1; //retorn 1 para indicar um erro.
+    }
+
+    int totalLivros = 0;
+    int totalEmprestimos = 0;
+    int opcao;
+
+    printf("Teste");
+ }
