7b37dedcc9569f67de6f0c2bd968173739aa8093
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-18 14:59:27 -0300

    Comecei a função principal
    fiz as funções nescessárias como a void e as struct e agora comecei a função principal.

--- Subirprogit/biblioteca2(Beta).c
+++ Subirprogit/biblioteca2(Beta).c
@@ -1,17 +1,42 @@
-
+// ===========================================================================
+// PRÁTICA: SISTEMA DE BIBLIOTECA - PARTE 2
+// 
+// OBJETIVO DESTA PARTE:
+// Adicionar a funcionalidade de empréstimo e introduzir alocação dinâmica.
+// - Os arrays de livros e empréstimos são alocados com malloc/calloc.
+// - NOva struct para Empréstimo.
+// - Liberação de memória com free.
+// ===========================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// --- Constantes Globais ---
 #define MAX_LIVROS 50
-#define MAX_EMPRESTIMOS 100
+#define MAX_EMPRESTIMOS 100 // Definimos uma capacidade de empréstimos.
 #define TAM_STRING 100
 
-struct Livro{
+// --- Definição das Estruturas ---
+// Struct Livro agora tem um campo 'disponivel' para controlar o status.
+struct Livro {
     char nome[TAM_STRING];
     char autor[TAM_STRING];
     char editora[TAM_STRING];
     int edicao;
-    int disponivel;
+    int disponivel;// 1 para sim, 0 para não.
+};
+
+//Nova struct para armazenar informações do empréstimo.
+struct Emprestimo {
+    int indiceLivro; //Para saber qual livro do array 'biblioteca' foi emprestado.
+    char nomeUsuario[TAM_STRING];
+};
+
+// --- Funcão para limpar o buffer de entrada ---
+void LimparBufferEntrada(){
+    int c;
+    while ((c = getchar ()) != '\n' && c != EOF);
 }
+
+// --- Função Principal (main) ---
