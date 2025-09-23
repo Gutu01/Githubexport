e4430cc879b8983f1e80453c42f4d22df53c6920
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-23 11:39:13 -0300

    Inserir número de jogadores
    Estou fazendo o código de forma a realmente usar uma memória dinâmica, não fazer uma memória estática de ainda depois fazer uma dinâmica.

---
+++ Subirprogit/War(Beta2).c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define max_string 50
+
+typedef struct{
+
+    char nome[max_string];
+    char cor[max_string];
+    int num_tropas;
+
+}Dados;
+
+void limparbufferentrada(){
+    int c;
+    while (( c = getchar ()) != '\n' && c != EOF);
+}
+
+int main(){
+
+    int jogadores=0;
+
+    Dados *dados;
+
+    dados = (Dados *) calloc(6, sizeof(Dados));
+
+    do{
+        printf("\nInsira o número de jogadores ou 0 para sair: ");
+        scanf("%d", &jogadores);
+        limparbufferentrada();
+
+        if(jogadores == 0){
+            printf("\nSaindo\n\n");
+            return 1;
+        }else if(jogadores<3 || jogadores>6)
+            printf("\nnúmero de jogadores inválido!\n");
+    }while (jogadores<3 || jogadores>6);
+
+    
+    return 0;
+}
