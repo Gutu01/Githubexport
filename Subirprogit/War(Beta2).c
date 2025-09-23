090f811cd16be93926c9d3a119e44c657205d46d
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-23 12:54:45 -0300

    Inserir quantos territórios quiser
    Terminei o sistema de escolhar quantos territórios quiser de acordo com o mínimo e o máximo pertimitdo. Além de registrar esses territórios e suas caracteristicas

--- Subirprogit/War(Beta2).c
+++ Subirprogit/War(Beta2).c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-#define max_string 50
+#define max_string 30
+#define max_nome_cor 10
 
 typedef struct{
 
     char nome[max_string];
-    char cor[max_string];
+    char cor[max_nome_cor];
     int num_tropas;
 
 }Dados;
@@ -18,7 +21,7 @@ void limparbufferentrada(){
 
 int main(){
 
-    int jogadores=0;
+    int jogadores=0, i;
 
     Dados *dados;
 
@@ -36,6 +39,32 @@ int main(){
             printf("\nNúmero de jogadores inválido!\n");
     }while (jogadores<3 || jogadores>6);
 
+    for(i=0; i < jogadores; i++){
+
+        printf("\nNome do %dº território: ", i+1);
+        fgets(dados[i].nome, max_string, stdin);
+        dados[i].nome[strcspn(dados[i].nome, "\n")] = '\0';
+
+        printf("Cor das tropas: ");
+        fgets(dados[i].cor, max_string, stdin);
+        dados[i].cor[strcspn(dados[i].cor, "\n")] = '\0';
+
+        printf("Número de tropas: ");
+        scanf("%d", &dados[i].num_tropas);
+        limparbufferentrada();
+        
+    }
+
+    printf("\n\nMapa de War\n\n");
+
+    for(i=0; i < jogadores; i++){
+        printf("%d - %s (cor %s, %d tropas)\n", i + 1, dados[i].nome, dados[i].cor, dados[i].num_tropas);
+    }
+
+    printf("\nTurno de ataque\n");
+    printf("Escolha um atacante");
     
+    free(dados);
+
     return 0;
 }
